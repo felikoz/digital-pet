4b410ac60a5f4d4133c4eebf93ee7fcd6e1570fa
felikoz <118719460+felikoz@users.noreply.github.com>
2023-05-24 07:19:11 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 import time 
 import random as rd
-money = 0
+money = 10000000
+inventory = {
+    "food" : 0,
+    "premium_food" : 0,
+    "toy "  : 0,
+    "premium_toy" : 0
 
+}
 class pet :
     def __init__(self, name , age , hunger , hapinnes,alive,type) :
         self.name = name 
@@ -41,6 +47,11 @@ class pet :
         if self.hunger <=20 :
             print("he hungy is so he grogy")
             self.hapiness -= 10
+    
+    def giveType(self):
+        petty = input(f"what is the pet type")
+        print(f"your pet type is {petty}")
+    
 
 
 
@@ -59,22 +70,76 @@ def work (money):
 
 
 
+def feed(inventory):
+    print("")
+
+
+
+def shop (money, inventory):
+    print("-----------------------------------------------------------------------------------")
+    print("your inventory ")
+    print(inventory)
+    print("-----------------------------------------------------------------------------------")
+    print ("items in shop")
+    print("1:food :15$")
+    print("2:premium food : 30$")
+    print ("3:toy :50$")
+    print ("4:premium toy : 100$")
+    itembought = input("what is the item you want to buy")
+    match itembought:
+        case "1": 
+            if money >= 15:
+                money -= 15 
+                inventory["food"] += 1 
+                print("you bought food for 15$")
+                print(f"you now own food and your balance is {money}")
+        case "2": 
+            if money >= 30:
+                money -= 30 
+                inventory["premium_food"] += 1 
+                print("you bought  premium food for 30$")
+                print(f"you now own premium food and your balance is {money}")
+        case "3":
+            if money >= 50:
+                money -= 50
+                inventory["toy"] += 1 
+                print("you bought toy for 50$")
+                print(f"you now own a toy and you balance is {money}")
+        case "4":
+            if money >= 100:
+                money -= 100
+                inventory["premium_toy"] += 1
+                print("you bought premium toy for 100$")
+                print(f"you now own a premium toy and your balance is {money}")
+        case _:
+            print("this is not a valid action")
 
 def action ():
+    print("----------------------------------------------------------------")
     print("what would you like to do")
     print("work:1")
     print("play with your pet:2")
     print("buy something :3")
     print("save and leave ?:4")
-    useraction = input("?         ")
+    print("feed your pet :5")
+    print("----------------------------------------------------------------")
+
+    useraction = input("?"   )
+    
     match useraction:
-        case 1:
+        case "1":
             work(money)
-        case 2: 
+        case "2": 
             print("your pet is now a bit happier ")
             cat.hapiness += 10
-        case 3:
+        case "3":
             print(f"you have {money}$")
+            shop(money,inventory)
+        case "5":
+            print("")
+        case _:
+            print("this is not a valid action")
+
             
 
 
@@ -85,12 +150,14 @@ cat = pet("",1,100,100,True,"")
 
 
 while cat.alive ==True :
-    if cat.name == "":
+    if cat.name == "" :
         cat.giveName()
     cat.hungerdown()
     cat.died()
     cat.aging()
-    work(money)
-    time.sleep(30)
+    action()
+
+    
+    
 
     
